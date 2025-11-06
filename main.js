cf3145d2482ddb6a944b3ceb363120daae3f9fe0
ricardoSo-sd <rsandovalort@gmail.com>
2025-11-05 19:59:22 -0500

    Ejercicios 3 y 5 agregados. Taller completo

--- main.js
+++ main.js
@@ -1,30 +1,67 @@
 //Ejercicio 1
 import {PI, IVA, DIAS_SEMANA} from "./constantes.js";
-import { areaCirculo, precioConIva, sumar, restar, multiplicar} from "./operaciones-matematicas.js";
+import { areaCirculo, precioConIva, sumar, restar, multiplicar, calcularTotal, aplicarImpuestos, aplicarDescuento, aplicarDescuentoPorRol} from "./operaciones-matematicas.js";
 
-
-console.log(DIAS_SEMANA[2])
-console.log(areaCirculo(5))
-console.log(precioConIva(100))
+console.log("\n---Constantes---")
+console.log("El tercer día de la semana es: ",DIAS_SEMANA[2])
+console.log("El área del círculo es: ",areaCirculo(5))
+console.log("El precio con IVA del producto que cuesta 100 es: ",precioConIva(100))
 
 //EJERCICIO 2
+console.log("\n---Operaciones matemáticas---")
+console.log("La suma de los números es: ",sumar(5,4))
+console.log("La resta de los números es: ",restar(5,4))
+console.log("La multiplicación de los números es: ",multiplicar(5,4))
 
-console.log(sumar(5,4))
-console.log(restar(5,4))
-console.log(multiplicar(5,4))
+//Ejercicio 3
+const producto1 = calcularTotal(1000, 2);
+const producto2 = calcularTotal(500, 1);  
+const producto3 = calcularTotal(800, 1);  
+const total = producto1 + producto2 + producto3;
 
-//para el 4to ejercicio
-import { ROLES } from './lab_5_javascript/usuariosConstantes.js';
-import { crearUsuario, esAdmin } from './lab_5_javascript/usuarios.js';
+console.log("\n---Cálculo de impuestos---")
 
-// Crear usuarios
+console.log("Total sin impuestos:", total);
+
+const totalConImpuestos = aplicarImpuestos(total);
+console.log("Total con impuestos:",totalConImpuestos);
+const totalFinal = aplicarDescuento(totalConImpuestos)
+console.log("Resultado final a pagar:", totalFinal)
+
+
+//Ejercicio 4
+import { ROLES } from './usuariosConstantes.js';
+import { crearUsuario, esAdmin } from './usuarios.js';
+
 const usuario1 = crearUsuario("Daniel", ROLES.ADMIN);
 const usuario2 = crearUsuario("Ricardo", ROLES.CLIENTE);
 
-// Mostrar información
 console.log("\n--- Información de Usuarios ---");
 console.log("Usuario 1:", usuario1);
 console.log("¿Pedro es admin?", esAdmin(usuario1));
 
 console.log("Usuario 2:", usuario2);
 console.log("¿Carlos es admin?", esAdmin(usuario2));
+
+//Ejercicio 5
+console.log("\n---Cuentas del admin---")
+
+const adminSubtotal = calcularTotal(25, 4);
+console.log("El subtotal del admin es: ", adminSubtotal);
+
+const adminImpuestos = aplicarImpuestos(adminSubtotal);
+console.log("El subtotal del admin con impuestos es: ", adminImpuestos);
+
+const adminValorTotal = aplicarDescuentoPorRol(adminImpuestos, "admin");
+console.log("El total a pagar del admin es: ", adminValorTotal);
+
+console.log("\n---Cuentas del cliente---")
+
+const clienteSubtotal = calcularTotal(50, 3);
+console.log("El subtotal del admin es: ", clienteSubtotal);
+
+const clienteImpuestos = aplicarImpuestos(clienteSubtotal);
+console.log("El subtotal del admin con impuestos es: ", clienteImpuestos);
+
+const clienteValorTotal = aplicarDescuentoPorRol(clienteImpuestos, "cliente");
+console.log("El total a pagar del admin es: ", clienteValorTotal);
--- operaciones-matematicas.js
+++ operaciones-matematicas.js
@@ -1,5 +1,5 @@
 //Ejercicio 1
-import {PI, IVA} from "./constantes.js";
+import {PI, IVA, DESCUENTO} from "./constantes.js";
 
 export function areaCirculo(radio){
     return (radio**2)*PI;
@@ -16,11 +16,30 @@ export function restar(a,b){
     return a-b;
 }
 export function multiplicar(a,b){
-    return a+b;
+    return a*b;
+}
+
+//Ejercicio 3
+export function calcularTotal(precio, cantidad) {
+    return precio * cantidad;
+    
+}
+export function aplicarImpuestos(total) {
+    return total + total * IVA;
+}
+export function aplicarDescuento(total) { 
+    return total - total * DESCUENTO;
 }
 
 
-
+//Ejercicio 5
+export function aplicarDescuentoPorRol(total, usuario){
+    if (usuario === "admin"){
+        return total - (total*(DESCUENTO*2));
+    }else{
+        return  total - (total*DESCUENTO)
+    }
+}
 
 
 
